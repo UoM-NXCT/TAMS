c0cc0afb54b2cc6aad33cd12cb1f2aa8e77754c5
tjkuson <tjkuson@mailbox.org>
2022-11-04 15:00:11 +0000

    Add instrument to scan creation form

--- client/dialogues/create_scan_dialogue.py
+++ client/dialogues/create_scan_dialogue.py
@@ -40,7 +40,7 @@ class CreateScanDialogue(QDialog):
 
         header_label: QLabel = QLabel("Create new scan")
 
-        # Get project id options
+        # Get project ID options
         self.new_scan_project_id_entry = QComboBox()
         with psycopg.connect(self.connection_string) as conn:
             with conn.cursor() as cur:
@@ -51,12 +51,27 @@ class CreateScanDialogue(QDialog):
                     f"{tuple_value[0]} ({tuple_value[1]})"
                     for tuple_value in raw_project_ids
                 ]
-                print(project_ids)
         self.new_scan_project_id_entry.addItems(project_ids)
+
+        # Get instrument ID options
+        self.new_scan_instrument_id_entry = QComboBox()
+        with psycopg.connect(self.connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute("select instrument_id, name from instrument;")
+                raw_instrument_ids: list[tuple[int, str]] = cur.fetchall()
+                # Hack the output into a value the Qt ComboBox likes (a list of strings)
+                instrument_ids: list[str] = [
+                    f"{tuple_value[0]} ({tuple_value[1]})"
+                    for tuple_value in raw_instrument_ids
+                ]
+        self.new_scan_instrument_id_entry.addItems(instrument_ids)
 
         # Arrange QLineEdit widgets in a QFormLayout
         dialogue_form = QFormLayout()
         dialogue_form.addRow("New scan project id:", self.new_scan_project_id_entry)
+        dialogue_form.addRow(
+            "New scan instrument id:", self.new_scan_instrument_id_entry
+        )
 
         # Make create project button
         create_scan_button = QPushButton("Create new scan")
@@ -75,7 +90,6 @@ class CreateScanDialogue(QDialog):
     def accept_project_info(self) -> None:
         """Read input data and save to database."""
 
-        # Check that the project id matches a project id in the database
         def project_id_exists(project_id: int) -> bool:
             """Check if a project with a given project id exists in the database."""
             with psycopg.connect(self.connection_string) as conn:
@@ -91,9 +105,32 @@ class CreateScanDialogue(QDialog):
                 )
             return bool(rows)
 
+        def instrument_id_exists(instrument_id: int) -> bool:
+            """Check if a project with a given instrument id exists in the database."""
+
+            with psycopg.connect(self.connection_string) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        f"select instrument_id from instrument where instrument_id={instrument_id}"
+                    )
+                    rows: list = cur.fetchall()
+
+            # Returns false if the list is empty (no rows returned)
+            if len(rows) > 1:
+                logging.warning(
+                    "Multiple instrument with the same instrument id; this shouldn't happen!"
+                )
+
+            return bool(rows)
+
+        # Get IDs from the combo boxes
         selected_project_id: int = int(
             self.new_scan_project_id_entry.currentText().split()[0]
         )
+        selected_instrument_id: int = int(
+            self.new_scan_instrument_id_entry.currentText().split()[0]
+        )
+
         if not project_id_exists(selected_project_id):
             logging.warning(
                 "Tried to create a scan with a project id that does not exist."
@@ -104,11 +141,21 @@ class CreateScanDialogue(QDialog):
                 "Project id does not exist. Please check inputs.",
                 QMessageBox.StandardButton.Ok,
             )
+        elif not instrument_id_exists(selected_instrument_id):
+            logging.warning(
+                "Tried to create a scan with an instrument ID that does not exist."
+            )
+            QMessageBox.warning(
+                self,
+                "Invalid instrument ID",
+                "Instrument ID does not exist. Please check inputs.",
+                QMessageBox.StandardButton.Ok,
+            )
         else:
             with psycopg.connect(self.connection_string) as conn:
                 with conn.cursor() as cur:
                     cur.execute(
-                        f"insert into scan (project_id) values ({selected_project_id});"
+                        f"insert into scan (project_id, instrument_id) values ({selected_project_id}, {selected_instrument_id});"
                     )
                     conn.commit()
                     logging.info("Created and committed scan to database.")
