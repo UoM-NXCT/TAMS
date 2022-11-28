26ee6931b5908a77159708d42d34353a8b149a2d
tjkuson <tjkuson@mailbox.org>
2022-11-28 17:25:26 +0000

    Update widget.py

--- client/widgets/metadata_panel/widget.py
+++ client/widgets/metadata_panel/widget.py
@@ -27,6 +27,40 @@ from client import settings
 from client.widgets.thumbnail import Thumbnail
 
 
+def get_thumbnail(prj_id: int, scan_id: int | None = None) -> Path:
+    """Return the first image in the scan directory as a thumbnail."""
+
+    # Get the path to the local library directory
+    local_lib: Path = Path(settings.get_lib("local"))
+
+    if scan_id:
+        # Get the path to the local scan directory
+        scan_dir: Path = local_lib / str(prj_id) / str(scan_id)
+    else:
+        # If no scan ID is provided, get the path to the local project directory
+        scan_dir = local_lib / str(prj_id)
+
+    # List of file extensions to search for; order matters!
+    extensions: tuple[str, ...] = (
+        "tiff",
+        "tif",
+        "bmp",
+        "jpg",
+        "jpeg",
+        "png",
+        "gif",
+    )
+
+    # Search recursively for the first image in the scan directory
+    for ext in extensions:
+        images: tuple[Path, ...] = tuple(scan_dir.rglob(f"*.{ext}"))
+        if images:
+            return images[0]
+
+    # If no images are found, return the placeholder image
+    return settings.placeholder_image
+
+
 class MetadataPanel(QWidget):
     """Display metadata on current selection."""
 
@@ -251,37 +285,3 @@ class MetadataPanel(QWidget):
         self.update_thumbnail()
         self.update_readme()
         self.layout().update()
-
-
-def get_thumbnail(prj_id: int, scan_id: int | None = None) -> Path:
-    """Return the first image in the scan directory as a thumbnail."""
-
-    # Get the path to the local library directory
-    local_lib: Path = Path(settings.get_lib("local"))
-
-    if scan_id:
-        # Get the path to the local scan directory
-        scan_dir: Path = local_lib / str(prj_id) / str(scan_id)
-    else:
-        # If no scan ID is provided, get the path to the local project directory
-        scan_dir = local_lib / str(prj_id)
-
-    # List of file extensions to search for; order matters!
-    extensions: tuple[str, ...] = (
-        "tiff",
-        "tif",
-        "bmp",
-        "jpg",
-        "jpeg",
-        "png",
-        "gif",
-    )
-
-    # Search recursively for the first image in the scan directory
-    for ext in extensions:
-        images: tuple[Path, ...] = tuple(scan_dir.rglob(f"*.{ext}"))
-        if images:
-            return images[0]
-
-    # If no images are found, return the placeholder image
-    return settings.placeholder_image
