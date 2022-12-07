9ae881755742c7a0af0df1900dbf58721229cbed
tjkuson <tjkuson@mailbox.org>
2022-12-07 16:01:38 +0000

    Update __init__.py

--- client/actions/__init__.py
+++ client/actions/__init__.py
@@ -1,6 +1,6 @@
 from .download import download
 from .open import open_data
 from .toggle_full_screen import toggle_full_screen
+from .update_table import update_table
 from .upload import upload
 from .validate import validate
-from .update_table import update_table
